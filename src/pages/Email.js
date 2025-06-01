429a08cad0576330acfa4fab80087d3284c02e01
Ayomiposi BALOGUN <balpos07@gmail.com>
2025-06-01 15:21:18 -0700

    email-padding-top

--- src/pages/Email.js
+++ src/pages/Email.js
@@ -353,7 +353,7 @@ export default function Email({ isSidebarOpen, isDarkMode }) {
         }
 
         .content-wrapper {
-          padding: 0px 32px;
+          padding: 32px 32px;
           max-width: 1200px;
           margin: 0 auto;
         }
